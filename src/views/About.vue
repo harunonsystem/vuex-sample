d63b0b85971c939d29c0866004f67649412a245d
harunon <harunonsystem@gmail.com>
2019-09-19 15:21:46 +0900

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h5>count.vue の数字が反映されてるぅ٩(๑´0`๑)۶</h5>
+    {{ $store.state.count }}
   </div>
 </template>
---
+++ src/views/Count.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="count">
+      <h1>This is an count page</h1>
+      <h5>クリックすると数字変わるよ(´・ω・`)</h5>
+      {{ $store.state.count }}
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        increment() {
+            this.$store.commit("increment");
+        },
+        decrement() {
+            this.$store.commit("decrement");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
